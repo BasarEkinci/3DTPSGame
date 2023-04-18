ded88301755b914176eac8c19a080c924f500d5e
BasarEkinci <basarekinci02@outlook.com>
2023-04-18 18:06:02 +0300

    Enemy
    Enemy Added

---
+++ 3DTPSGame/Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPSGame.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        
+    }    
+}
+
